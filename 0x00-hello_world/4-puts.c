151cba4bdeeb3d5218b8041d2e71081d4e96b6c1
destycoco <destibright1@gmail.com>
2023-03-17 06:10:24 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - A program that prints out a sentence followed by a new line
+ * return: (0)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
