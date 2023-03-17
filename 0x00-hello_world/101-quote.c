59335ad9aa74a2d092be3a99414d5aba449ebee9
destycoco <destibright1@gmail.com>
2023-03-17 08:54:58 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
- * main - A program that prints a line of code to the std error
+/**
+ * main - A program that prints a line of code to the standard error
  *
  * Return: 1
 */
