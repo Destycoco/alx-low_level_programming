6bbf3dff641e101179bc670a4de599fc3ba0ca31
destycoco <destibright1@gmail.com>
2023-03-17 06:24:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints out a sentence with puts function
- * return:0
+ * return - 0
 */
-int main(void)
+int main(void);
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
