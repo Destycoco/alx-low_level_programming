2c63ad18eecf8ca9856d5b6046ed5274db57c920
destycoco <destibright1@gmail.com>
2023-03-17 06:26:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that prints out a sentence with puts function
  * return - 0
 */
-int main(void);
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
