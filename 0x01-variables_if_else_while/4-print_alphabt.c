1d22960360c09b4f2a6d374884b3ff3b99dbb056
destycoco <destibright1@gmail.com>
2023-03-18 16:07:13 -0700

    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,11 @@
 int main(void)
 {
 	char c;
-	char x;
-	char y;
 
 	c = 'a';
-	x = 'e';
-	y = 'q';
 	while (c <= 'z')
 	{
-		if ((c != x && c != y) && c <= 'z')
+		if ((c != 'e' && c != 'q') && c <= 'z')
 		{
 			putchar(c);
 			c++;
