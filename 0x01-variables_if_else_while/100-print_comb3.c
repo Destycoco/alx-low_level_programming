c14fbe2056ce271ada5fbb1ac10f4e43fbca5188
destycoco <destibright1@gmail.com>
2023-03-19 18:40:29 -0700

    prints two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,8 @@ int i;
 int j;
 
 for (i = 0; i < 10; i++)
-for (j = 0; j < 10; j++)
+{
+for (j = 1; j < 10; j++)
 {
 if 
 	(i < j && i != j)
@@ -20,10 +21,10 @@ putchar(j + '0');
 }
 if 
 	(i + j != 17)
-{
 putchar(',');
 putchar(' ');
 }
 }
+putchar('\n');
 return (0);
 }
