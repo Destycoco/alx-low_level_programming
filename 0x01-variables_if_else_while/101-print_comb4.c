6d101e16f802e1ec29ca6248ba33eee20f7cdf0b
destycoco <destibright1@gmail.com>
2023-03-20 08:58:35 -0700

    prints 3 numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int i;
 int j;
 int k;
 
-for(i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
-for(j = 0; j < 10; j++)
+for (j = 0; j < 10; j++)
 {
-for(k = 0; k < 10; k++)
+for (k = 0; k < 10; k++)
 {
 if (i < j && i < k && j < k && i != j && i != j && j != k)
 {
@@ -22,7 +22,7 @@ putchar(i + '0');
 putchar(j + '0');
 putchar(k + '0');
 {
-if (i+j+k != 24)
+if (i + j + k != 24)
 {
 putchar(',');
 putchar(' ');
