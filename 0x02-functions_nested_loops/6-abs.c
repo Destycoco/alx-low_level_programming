ccfe2efaad816dd80b0693638a935d7bd0659ab3
destycoco <destibright1@gmail.com>
2023-03-22 17:47:27 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the abs value of n
+ *
+ *
+ * @r: the character  that is checked
+ * Return: the abs of int
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+		return (-r);
+}
