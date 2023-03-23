4d2a3e8a52d080694e36770d9914a32f88a42d78
destycoco <destibright1@gmail.com>
2023-03-23 06:13:29 -0700

    hard 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,22 +7,11 @@
  */
 void print_to_98(int n)
 {
+	
 if (n <= 98)
 {
 for (; n <= 98; n++)
-if (n == 98)
 {
-printf("%d ", n);
-printf('\n')
-break;
-}
-else
-{
-printf("%d ", n);
-}
-}
-else
-for (; n >= 98; n--)
 if (n == 98)
 {
 printf("%d ", n);
@@ -32,5 +21,23 @@ break;
 else
 {
 printf("%d ", n);
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d ", n);
+printf('\n');
+break;
+}
+else
+{
+printf("%d ", n);
+}
+}
+}
 }
 }
