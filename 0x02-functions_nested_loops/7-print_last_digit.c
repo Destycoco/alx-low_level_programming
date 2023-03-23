6daede28b55dd924737c106a355e58b28bb8e294
destycoco <destibright1@gmail.com>
2023-03-22 18:33:47 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	 int ld;
-	 ld = n % 10;
-	 if (n < 0)
-		 ld = -ld;
-	 else
-		 ld = ld;
+	int ld;
+
+	ld = n % 10;
+	if (n < 0)
+		ld = -ld;
+	else
+		ld = ld;
 	 _putchar(ld + '0');
 return (ld);
 }
