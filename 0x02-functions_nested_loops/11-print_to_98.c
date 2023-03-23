88c39a826567230a21b85ec6b5e70c44fba4ffe1
destycoco <destibright1@gmail.com>
2023-03-23 06:08:08 -0700

    hard 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,48 +1,36 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - a function that prints to 98
  * @n: character to be observed
- * @i: charcter
  * Return: always 0
  */
 void print_to_98(int n)
 {
-int i;
-
-if (n > 0 && n <= 98)
+if (n <= 98)
 {
-for (i = n; i <= 98; i++)
+for (; n <= 98; n++)
+if (n == 98)
 {
-_putchar(i);
-_putchar(',');
-_putchar(' ');
+printf("%d ", n);
+printf('\n')
+break;
 }
-_putchar('\n');
-}
-else if (n <= 0)
+else
 {
-for (i = n; i <= 98; i++)
-{
-_putchar(n);
-_putchar(',');
-_putchar(' ');
+printf("%d ", n);
 }
-_putchar ('\n');
 }
-else if (n > 98)
+else
+for (; n >= 98; n--)
+if (n == 98)
 {
-for (i = n; n > 98; i--)
-{
-_putchar(n);
-_putchar(',');
-_putchar(' ');
+printf("%d ", n);
+printf('\n');
+break;
 }
-_putchar ('\n');
-}
-else if (n == 98)
+else
 {
-_putchar(n);
-_putchar(',');
-_putchar(' ');
+printf("%d ", n);
 }
 }
