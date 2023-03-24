ecb999e4fcc50d2f44c8658b94cf98d46892a5e5
destycoco <destibright1@gmail.com>
2023-03-24 09:33:10 -0700

    multiplication of 2 nos

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * nul - multiplies two numbers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Always 0
+ */
+int nul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
