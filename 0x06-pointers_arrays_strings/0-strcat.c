bb87d5bddc4afbcf0b1a4687563bdac624e3a585
destycoco <destibright1@gmail.com>
2023-03-31 01:09:13 +0100

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
  * *_strcat - concatenates @src to @dest
  * @src: the source string to append to @dest
  * @dest: the destiation string to be concatenated upon
- * Return: char
  * Return:pointer to the resulting string
  */
 
@@ -11,6 +11,7 @@ char *_strcat(char *dest, char *src)
 {
 	int index = 0;
 	int dest_len = 0;
+
 	while (dest[index++])
 		dest_len++;
 	for (index = 0; src[index]; index++)
